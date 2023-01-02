35771fa36a8be9862f050496dba9ae89bea427c5
Pedro Pereira <palex@cc.isel.ipl.pt>
2023-01-02 19:51:49 +0000

    UpdateDay16

--- src/Day16.kt
+++ src/Day16.kt
@@ -11,23 +11,23 @@ const val REST = """([A-Z, ]+)"""
 val valvePattern = Regex("""Valve $NAME has flow rate=$INT; tunnels? leads? to valves? $REST$""")
 
 data class Valve(val name: String, val rate: Int, val to: List<String>) : Comparable<Valve>{
+    override fun hashCode() = name.hashCode()
     override fun compareTo(other: Valve) = compareValuesBy(this,other) { it.name }
-    var tunnels: List<Valve> = emptyList()
-        private set
-    val pathsToRated by lazy { getPathsToValidValves(this) }
+    private var tunnels: List<Valve> = emptyList()
+    var tunnelsToRated: Map<Valve,Int> = emptyMap()
 
-    fun buildTunnels(valves: List<Valve>) { tunnels = to.map { name -> valves.first { it.name == name } }}
-
-    private fun getPathsToValidValves(from: Valve, dist: Int = 0, visited: Set<Valve> = setOf(from)): Map<Valve,Int> =
-        buildMap {
-            for (valve in from.tunnels.filter { it !in visited }) {
-                if (valve.rate != 0) set(valve, dist + 1)
-                for ((v, d) in getPathsToValidValves(valve, dist + 1, visited + valve)) {
-                    val dd = get(v)
-                    set(v, if (dd == null) d else min(d, dd))
-                }
-            }
+    fun buildTunnels(valves: Map<String,Valve>) { tunnels = to.map { name -> checkNotNull(valves[name]) } }
+    fun buildRatedTunnels() {
+        fun build(from: Valve, dist: Int, visited: Set<Valve>, to: MutableMap<Valve,Int>): Map<Valve,Int> {
+            val rated = from.tunnels
+                .filter { it !in visited }
+            rated.filter { it.rate > 0 }
+                .forEach { to[it] = minOf(dist+1, to[it] ?: Int.MAX_VALUE) }
+            rated.forEach { build(it, dist+1, visited+from, to) }
+            return to
         }
+        tunnelsToRated = build(this, 0, emptySet(), mutableMapOf())
+    }
 }
 
 fun Valve(line: String): Valve {
@@ -35,77 +35,97 @@ fun Valve(line: String): Valve {
     return Valve(n, r.toInt(), t.split(", "))
 }
 
-data class State(
-    val time: Int,
-    val curr: Valve,
-    val open: List<Valve> = emptyList(),
-    val pressure: Int = 0,
-    val timeElephant: Int = 0,
-    val currElephant: Valve = curr,
-) : Comparable<State> {
-    override fun compareTo(other: State) = compareValuesBy(this,other){ -it.pressure }
+typealias Valves = Map<String,Valve>
+
+fun Valves(lines: List<String>): Valves {
+    val valves = lines.map { Valve(it) }.associateBy { it.name }
+    valves.values.forEach { it.buildTunnels(valves) }
+    valves.values.forEach { it.buildRatedTunnels() }
+    return valves
 }
 
-fun part1(lines: List<String>): Int {
-    val valves = lines.map { Valve(it) }
-    valves.forEach { it.buildTunnels(valves) }
+val Valves.rated get() = values.filter { it.rate > 0 }.sortedBy { -it.rate }
 
-    val init = valves.first { it.name=="AA" }
-    val queue = PriorityQueue<State>().also { it.add(State(30, init)) }
+data class State(
+    val time: Int,  // Remaining time
+    val curr: Valve,
+    val opened: List<Valve> = emptyList(),
+    val closed: List<Valve>,
+    val pressure: Int = 0,
+    val timeElephant: Int = 0,          // Part2
+    val currElephant: Valve = curr,     // Part2
+) : Comparable<State> {
+    private val totalPressure: Int = pressure + estimateClosed()
+    override fun compareTo(other: State) = compareValuesBy(other,this){ it.totalPressure }
+    private fun estimateClosed(): Int {
+        var valve = curr
+        var tm = time
+        var press = 0
+        closed.forEach { v ->
+            tm -= valve.tunnelsToRated[v]!! + 1
+            press += v.rate * tm
+            valve = v
+        }
+        return press
+    }
+    override fun toString() = "State($time,${curr.name},opened=${opened.map { it.name }},closed=${closed.map { it.name }},$pressure,$totalPressure)"
+}
+
+fun part1(valves: Valves): Int {
+    val open = PriorityQueue<State>()
+    open.add(State(30, valves["AA"]!!, closed = valves.rated ))
     var best = 0
     val visited: MutableMap<List<Valve>, Int> = mutableMapOf()
-    while (queue.isNotEmpty()) {
-        val state = queue.remove()
-        val (time, curr, open, pressure) = state
-        best = max(best, pressure)
-        val vis = visited[open]
-        if (vis!=null && vis >= pressure) continue
-        visited[open] = pressure
-        for ((next, dist) in curr.pathsToRated) {
-            val newTime = time-dist-1
-            if (newTime >= 0 && next !in open)
-                queue.add(State(newTime, next, (open + next).sorted(), pressure + next.rate * newTime))
+    while (open.isNotEmpty()) {
+        val state = open.remove()
+        best = max(best, state.pressure)
+        val vis = visited[state.opened]
+        if (vis!=null && vis > state.pressure) continue
+        visited[state.opened] = state.pressure
+        for ((next, dist) in state.curr.tunnelsToRated) {
+            val newTime = state.time-dist-1
+            if (newTime >= 0 && next !in state.opened)
+                open.add(State(newTime, next, (state.opened + next).sorted(), state.closed-next, state.pressure + next.rate * newTime))
         }
     }
-    println("Visited = ${visited.size}")
     return best
 }
 
-fun part2(lines: List<String>): Int {
-    val valves = lines.map { Valve(it) }
-    valves.forEach { it.buildTunnels(valves) }
-
-    val init = valves.first { it.name=="AA" }
-    val queue = PriorityQueue<State>().also { it.add(State(26, init, emptyList(), 0, 26, init)) }
+fun part2(valves: Valves): Int {
+    val init = valves["AA"]!!
+    val open = PriorityQueue<State>()
+    open.add(State(26, init, emptyList(), valves.rated, 0, 26, init))
     var best = 0
     val visited: MutableMap<List<Valve>, Int> = mutableMapOf()
-    while (queue.isNotEmpty()) {
-        val state = queue.remove()
-        var (time, curr, open, pressure, timeE, currE) = state
-        best = max(best, pressure)
-        val vis = visited[open]
-        if (vis!=null && vis >= pressure) continue
-        visited[open] = pressure
+    while (open.isNotEmpty()) {
+        val state = open.remove()
+        best = max(best, state.pressure)
+        val vis = visited[state.opened]
+        if (vis!=null && vis > state.pressure) continue
+        visited[state.opened] = state.pressure
+        var time = state.time
+        var curr = state.curr
+        var timeE = state.timeElephant
+        var currE = state.currElephant
         if (time < timeE) {
             time = timeE.also { timeE = time }
             curr = currE.also { currE = curr }
         }
-        for ((next, dist) in curr.pathsToRated) {
+        for ((next, dist) in curr.tunnelsToRated) {
             val newTime = time-dist-1
-            if (newTime >= 0 && next !in open)
-                queue.add(State(newTime, next, (open + next).sorted(), pressure + next.rate * newTime, timeE, currE))
+            if (newTime >= 0 && next !in state.opened)
+                open.add(State(newTime, next, (state.opened + next).sorted(), state.closed-next, state.pressure + next.rate * newTime, timeE, currE))
         }
     }
-    println("Visited = ${visited.size}")
     return best
 }
 
 fun main() {
-    val testInput = readInput("Day16_test")
-    check(part1(testInput) == 1651)
-    check(part2(testInput) == 1707)
+    val testValves = Valves(readInput("Day16_test"))
+    check(part1(testValves) == 1651)
+    check(part2(testValves) == 1707)
 
-    val input = readInput("Day16")
-    println(part1(input))  // 1789
-    println(part2(input))  // 2495
+    val valves = Valves(readInput("Day16"))
+    println(part1(valves))  // 1789
+    println(part2(valves))  // 2496
 }
